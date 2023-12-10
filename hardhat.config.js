b57dbf0d1f909f3ce20ba6612a0a5756da63ae3f
Elien Yang <eislander16@gmail.com>
2023-12-09 21:36:44 -0700

    update the deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('hardhat-gas-reporter');
+require('dotenv').config();
 
 // NEVER record important private keys in your code - this is for demo purposes
-const GOERLI_TESTNET_PRIVATE_KEY = '';
+const ARBITRUM_GOERLI_PRIVATE_KEY = process.env.ARBITRUM_GOERLI_KEY;
 const ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY = '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -15,7 +16,7 @@ module.exports = {
     arbitrumGoerli: {
       url: 'https://goerli-rollup.arbitrum.io/rpc',
       chainId: 421613,
-      //accounts: [GOERLI_TESTNET_PRIVATE_KEY]
+      // accounts: [ARBITRUM_GOERLI_PRIVATE_KEY],
     },
     arbitrumOne: {
       url: 'https://arb1.arbitrum.io/rpc',
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,32 @@
 const hre = require('hardhat');
 
 async function main() {
-  const checkers = await hre.ethers.deployContract('Checkers');
+  const stake = hre.ethers.parseEther('0.0001');
+
+  const checkers = await hre.ethers.deployContract(
+    'Checkers',
+    [
+      '0xBBAb0ef3CAb1F117809D8A4e3648a52e8295c1Cc',
+      500,
+      '0x5c6b02db8b672415ffad906d7ccee10bd53dbad7d0b29e2bc0e50c93d5f31093',
+    ],
+    {
+      value: stake,
+    }
+  );
+
   await checkers.waitForDeployment();
-  console.log(`Cupcake vending machine deployed to ${checkers.target}`);
+
+  console.log(
+    `Checkers with ${ethers.formatEther(stake)} ETH deployed to ${
+      checkers.target
+    }`
+  );
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
-  process.exit(1);
+  process.exitCode = 1;
 });
